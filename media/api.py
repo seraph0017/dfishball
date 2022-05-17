3977bed6362fd053c9215757318e789fcced0748
徐楠 <seraph0017@hotmail.com>
2022-05-17 09:23:15 +0800

    update

--- media/api.py
+++ media/api.py
@@ -18,12 +18,10 @@ class MediaGroupApi(viewsets.ModelViewSet):
     serializer_class = MediaGroupSerializer
 
 
-
 class MediaApi(viewsets.ModelViewSet):
 
     queryset = Media.get_all_active_photo()
     serializer_class = MediaSerializer
-
 
 
 # using
@@ -33,11 +31,11 @@ class TagListApi(generics.ListAPIView):
     serializer_class = TagSerializer
 
 
-
 class MediaGroupListApi(generics.ListAPIView):
 
     queryset = MediaGroup.get_all_active_group()
     serializer_class = MediaGroupSerializer
+
 
 class MediaListApi(APIView):
 
@@ -46,21 +44,22 @@ class MediaListApi(APIView):
         limit = int(request.query_params.get('limit', "10"))
         offset = int(request.query_params.get('offset', "0"))
         tags = request.query_params.get('tags', [])
-        media_list = Media.get_all_active_photo_by_tags(media_group_id, json.loads(tags), limit, offset)
+        media_list = Media.get_all_active_photo_by_tags(
+            media_group_id, json.loads(tags), limit, offset)
         serializer = MediaSerializer(media_list, many=True)
         return Response(serializer.data)
 
     def post(self, request):
         f = request.FILES.get("filePath")
         Media.create_media(f, request.POST.get(
-                'upload_user_id'), request.POST.get('group_id'))
+            'upload_user_id'), request.POST.get('group_id'))
         return Response(dict(message="ok"))
 
 
 class MediaDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Media.get_all_active_photo()
     serializer_class = MediaSerializer
-    lookup_field = 'media_id' 
+    lookup_field = 'media_id'
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -75,14 +74,15 @@ class MediaDetailApi(generics.RetrieveUpdateDestroyAPIView):
         for tag in obj.tags.all():
             obj.tags.remove(tag)
         for tag in tags:
-            obj.tags.add(tag)
+            if tag.strip():
+                obj.tags.add(tag)
         obj.pic_time = datetime.datetime.strptime(str_date, "%Y-%m-%d")
         obj.save()
         return Response(dict(message="ok"))
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
-    
+
     def get_object(self):
         queryset = self.get_queryset()
         filter = {}
@@ -90,4 +90,3 @@ class MediaDetailApi(generics.RetrieveUpdateDestroyAPIView):
         obj = get_object_or_404(queryset, **filter)
         self.check_object_permissions(self.request, obj)
         return obj
-
